34c83bc2c068dd4de15722fac5a3ae3464115f04
adarsh4853 <154544189+adarsh4853@users.noreply.github.com>
2024-03-16 12:55:39 +0530

    Some Modifications

--- RandomColorfulBoxes.js
+++ RandomColorfulBoxes.js
@@ -9,21 +9,13 @@ function getRandomColor(){
     return Math.floor(Math.random() * 1000) % 255;
 }
 
-document.getElementsByClassName("box1")[0].style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
-a = getRandomColor();
-b = getRandomColor();
-c = getRandomColor();
-document.getElementsByClassName("box2")[0].style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
-a = getRandomColor();
-b = getRandomColor();
-c = getRandomColor();
-document.getElementsByClassName("box3")[0].style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
-a = getRandomColor();
-b = getRandomColor();
-c = getRandomColor();
-document.getElementsByClassName("box4")[0].style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
-a = getRandomColor();
-b = getRandomColor();
-c = getRandomColor();
-document.getElementsByClassName("box5")[0].style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
+setInterval(() => {
+    for(let i=0; i<5; i++){
+        a = getRandomColor();
+        b = getRandomColor();
+        c = getRandomColor();
+        // document.getElementsByClassName(`box${i+1}`)[0].innerHTML = `hello ${i+1}`;
+        document.getElementsByClassName(`box${i+1}`)[0].style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
+    }
+}, 100)
 
